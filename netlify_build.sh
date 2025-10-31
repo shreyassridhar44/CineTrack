88f8f5a38dece4523717cb0ed11eb55c607b4062
Shreyas <shreyassridhar146@gmail.com>
2025-10-31 14:28:02 +0530

    Created .env file from environment variables during build

--- netlify_build.sh
+++ netlify_build.sh
@@ -2,17 +2,22 @@
 # This script tells the build process to exit immediately if any command fails.
 set -e
 
-# --- 1. INSTALL FLUTTER SDK ---
+# --- 1. CREATE THE .env FILE FROM NETLIFY'S ENVIRONMENT VARIABLE ---
+echo "Creating .env file..."
+echo "TMDB_API_KEY=${TMDB_API_KEY}" > .env
+echo ".env file created successfully."
+
+# --- 2. INSTALL FLUTTER SDK ---
 echo "Cloning Flutter repository..."
 git clone https://github.com/flutter/flutter.git --depth 1 --branch stable $HOME/flutter
 export PATH="$HOME/flutter/bin:$PATH"
 
-# --- 2. SETUP FLUTTER PROJECT ---
+# --- 3. SETUP FLUTTER PROJECT ---
 echo "Configuring Flutter and getting dependencies..."
 flutter config --enable-web
 flutter pub get
 
-# --- 3. BUILD THE FLUTTER WEB APP ---
+# --- 4. BUILD THE FLUTTER WEB APP ---
 echo "Building Flutter web..."
 flutter build web --release
 
